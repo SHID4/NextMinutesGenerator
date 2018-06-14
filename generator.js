014a5ad19b6cc9ab4694590548646e1add4f7ddd
ishidashota <c611621050@tokushima-u.ac.jp>
2018-06-14 11:35:21 +0900

    ドキュメント取得,次やるべきこと記述

--- generator.js
+++ generator.js
@@ -1,3 +1,10 @@
 function myFunction() {
   Logger.log("fugafuga");
+  var document = DocumentApp.getActiveDocument();//今開いているドキュメントを取得
+  var body = document.getBody;//本文取得
+  body.match(/次回のミーティング:.*¥n/);//次回のミーティング部分を取得
+//TO DO 上のミーティング部分から数字のものだけを取得して0101のような数値を取得
+  //それを次回の議事録のタイトルとして生成
+  //本文はそれをコピーしたもので
+  //上のメニューから議事録→次回議事録作成で次が作れるようにする
 }
