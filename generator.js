9b60f8a2a70dbe3c0921b115a23677481ccb6c05
ishidashota <c611621050@tokushima-u.ac.jp>
2018-06-15 23:44:48 +0900

    ドキュメント取得から必要文字列取得しその名前のドキュメントを作成（デバッグ未実行）

--- generator.js
+++ generator.js
@@ -2,7 +2,15 @@ function myFunction() {
   Logger.log("fugafuga");
   var document = DocumentApp.getActiveDocument();//今開いているドキュメントを取得
   var body = document.getBody;//本文取得
-  body.match(/次回のミーティング:.*¥n/);//次回のミーティング部分を取得
+  var nextMeeting = body.match(/次回のミーティング:.*時/);//次回のミーティング以降から何時までの部分の文字列をを取得
+  var nextDay = nextMeeting.match(/[^0-9^\.]/g);//数字だけ抽出
+
+  var newDoc = DocumentApp.create(nextDay);
+
+  
+
+
+
 //TO DO 上のミーティング部分から数字のものだけを取得して0101のような数値を取得
   //それを次回の議事録のタイトルとして生成
   //本文はそれをコピーしたもので
