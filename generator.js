0408ab29c6a9bce9bb4cd76cffea2d6d5440f1e6
ishidashota <c611621050@tokushima-u.ac.jp>
2018-06-16 18:30:44 +0900

    次回の議事録完成

--- generator.js
+++ generator.js
@@ -1,18 +1,35 @@
-function myFunction() {
-  Logger.log("fugafuga");
+//次回議事録の作成
+//次の日程を入力してメニューから「議事録→次の議事録作成」で次の議事録を作成する
+function GenerateNextMinutes() {
   var document = DocumentApp.getActiveDocument();//今開いているドキュメントを取得
-  var body = document.getBody;//本文取得
-  var nextMeeting = body.match(/次回のミーティング:.*時/);//次回のミーティング以降から何時までの部分の文字列をを取得
-  var nextDay = nextMeeting.match(/[^0-9^\.]/g);//数字だけ抽出
-
-  var newDoc = DocumentApp.create(nextDay);
-
+  var body = document.getBody();//本文取得
+  var nextMeeting = body.getText().match(/次回.*\n.*日/).toString();//次回のミーティング以降から何時までの部分の文字列をを取得
+  var month = nextMeeting.match(/.*月/).toString();
+      month =  month.replace(/[^0-9]/g,"").toString();
+  if(month.length == 1)　 month = '0' + month;
+  var date = nextMeeting.match(/月.*日/).toString();
+      date = date.replace(/[^0-9]/g,"").toString();
+  if(date.length == 1) date = '0' + date;
   
+  var nextDay = month + date;//数字だけ抽出
+  Logger.log(nextDay);
+  
+  //自分のファイル名を取得
+  var DocId = document.getId()
+  
+  var DocFile = DriveApp.getFileById(DocId);
+  DocFile.makeCopy(nextDay);
+   Logger.log(DocFile);
 
-
-
-//TO DO 上のミーティング部分から数字のものだけを取得して0101のような数値を取得
-  //それを次回の議事録のタイトルとして生成
-  //本文はそれをコピーしたもので
-  //上のメニューから議事録→次回議事録作成で次が作れるようにする
 }
+
+//起動した時に呼び出される関数
+  function onOpen(){
+    //まだUIについて良くわかってないけど以下の記述でメニューを追加できるみたい
+    var ui = DocumentApp.getUi();
+    var menu = ui.createMenu('議事録');
+    menu.addItem('次回の議事録を作成', 'GenerateNextMinutes');
+    menu.addToUi();
+    
+  }
+
